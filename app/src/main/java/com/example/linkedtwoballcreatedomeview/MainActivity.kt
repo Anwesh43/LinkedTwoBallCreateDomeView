7d11780beac46686889bb042ed11481b456c669b
Anwesh43 <anweshthecool0@gmail.com>
2021-06-21 10:27:11 +0530

    adding view in activity

--- app/src/main/java/com/example/linkedtwoballcreatedomeview/MainActivity.kt
+++ app/src/main/java/com/example/linkedtwoballcreatedomeview/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.linkedtwoballcreatedomeview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.example.twoballcreatedomeview.TwoBallCreateDomeView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        TwoBallCreateDomeView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
