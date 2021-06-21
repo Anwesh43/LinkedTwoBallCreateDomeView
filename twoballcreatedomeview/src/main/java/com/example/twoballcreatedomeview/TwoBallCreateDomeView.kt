d5ec207b5c7c23564c8109c0ab6185ea8673db2c
Anwesh43 <anweshthecool0@gmail.com>
2021-06-21 09:40:32 +0530

    adding constants and libraries

--- twoballcreatedomeview/src/main/java/com/example/twoballcreatedomeview/TwoBallCreateDomeView.kt
+++ twoballcreatedomeview/src/main/java/com/example/twoballcreatedomeview/TwoBallCreateDomeView.kt
@@ -1,2 +1,30 @@
 package com.example.twoballcreatedomeview
 
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Color
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.RectF
+
+val colors : Array<Int> = arrayOf(
+    "#f44336",
+    "#01579B",
+    "#00C853",
+    "#FFC107",
+    "#2962FF"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val scGap : Float = 0.02f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 3.9f
+val delay : Long = 20
+val deg : Float = 180f
+val startDeg : Float = 270f
+val sweepDeg : Float = 60f
+val backColor : Int = Color.parseColor("#BDBDBD")
+val rFactor : Float = 18.4f
